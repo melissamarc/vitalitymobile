b4468ca7d65c993aad471b85e28a97f7374c4dc8
unknown <melmarc1moreira@gmail.com>
2024-11-27 16:01:21 -0300

    tela de cadastro funcionando!!!!!!!!!

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,75 +1,156 @@
-// screens/SignupScreen.js
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Alert, Image, StyleSheet, StatusBar } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Alert, StyleSheet, StatusBar, Platform } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Picker } from '@react-native-picker/picker';
 import { firebase } from '../firebaseconfig';
-import { FontAwesome } from '@expo/vector-icons';
 
 export default function SignupScreen({ navigation }) {
-  const [name, setName] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [username, setUsername] = useState('');
+  const [dob, setDob] = useState(null);
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [gender, setGender] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const validatePassword = (password) => {
+    const regex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$/;
+    return regex.test(password);
+  };
+
+  const handleDateChange = (event, selectedDate) => {
+    setShowDatePicker(false);
+    if (selectedDate) {
+      setDob(selectedDate);
+    }
+  };
 
   const handleSignup = () => {
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(() => {
+    if (!fullName || !username || !dob || !gender || !email || !password || !confirmPassword) {
+      Alert.alert('Erro', 'Todos os campos devem ser preenchidos!');
+      return;
+    }
+
+    if (!validatePassword(password)) {
+      Alert.alert(
+        'Senha inválida',
+        'A senha deve conter pelo menos uma letra maiúscula, um número e ter no mínimo 6 caracteres.'
+      );
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      Alert.alert('Erro', 'As senhas não coincidem!');
+      return;
+    }
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const userId = userCredential.user.uid;
+
+        firebase.firestore().collection('users').doc(userId).set({
+          fullName,
+          username,
+          dob: dob ? dob.toISOString().split('T')[0] : '',
+          gender,
+          email,
+        });
+
         navigation.navigate('Dashboard');
       })
-      .catch(error => Alert.alert(error.message));
+      .catch((error) => Alert.alert('Erro', error.message));
   };
 
   return (
     <View style={styles.container}>
-      {/* Oculta a StatusBar */}
       <StatusBar hidden />
 
       <View style={styles.topSection}>
-        <Text style={styles.headerText}>Let's get started!</Text>
-        <Image source={{ uri: 'https://path_to_your_image' }} style={styles.image} />
+        <Text style={styles.headerText}>Criar Conta</Text>
       </View>
 
-      <View style={styles.signupBox}>
+      <View style={styles.form}>
         <TextInput
-          placeholder="Full Name"
-          value={name}
-          onChangeText={setName}
+          placeholder="Nome completo"
+          value={fullName}
+          onChangeText={setFullName}
           style={styles.input}
+          placeholderTextColor="black"
         />
         <TextInput
-          placeholder="Email Address"
+          placeholder="Nome de usuário"
+          value={username}
+          onChangeText={setUsername}
+          style={styles.input}
+          placeholderTextColor="black"
+        />
+
+        {/* Botão de calendário */}
+        <TouchableOpacity onPress={() => setShowDatePicker(true)} style={styles.input}>
+          <Text style={{ color: dob ? 'black' : '#888' }}>
+            {dob ? dob.toLocaleDateString() : 'Data de nascimento'}
+          </Text>
+        </TouchableOpacity>
+        {showDatePicker && (
+          <DateTimePicker
+            value={dob || new Date()}
+            mode="date"
+            display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+            textColor="black" // Apenas para iOS
+            onChange={handleDateChange}
+          />
+        )}
+
+        {/* Dropdown de gênero */}
+        <View style={styles.pickerContainer}>
+          <Picker
+            selectedValue={gender}
+            onValueChange={(itemValue) => setGender(itemValue)}
+            style={styles.picker}
+          >
+            <Picker.Item label="Selecione o gênero" value="" color="black" />
+            <Picker.Item label="Feminino" value="feminino" color="black" />
+            <Picker.Item label="Masculino" value="masculino" color="black" />
+            <Picker.Item label="Prefiro não dizer" value="naoDizer" color="black" />
+          </Picker>
+        </View>
+
+        <TextInput
+          placeholder="Email"
           value={email}
           onChangeText={setEmail}
           style={styles.input}
           keyboardType="email-address"
+          placeholderTextColor="black"
         />
         <TextInput
-          placeholder="Password"
+          placeholder="Senha"
           value={password}
           onChangeText={setPassword}
           style={styles.input}
           secureTextEntry
+          placeholderTextColor="black"
         />
+        <TextInput
+          placeholder="Confirmar senha"
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+          style={styles.input}
+          secureTextEntry
+          placeholderTextColor="black"
+        />
+
         <TouchableOpacity style={styles.signupButton} onPress={handleSignup}>
-          <Text style={styles.signupButtonText}>Sign Up</Text>
+          <Text style={styles.signupButtonText}>Cadastrar</Text>
         </TouchableOpacity>
 
-        <Text style={styles.orText}>Or</Text>
-
-        <View style={styles.socialButtons}>
-          <TouchableOpacity style={styles.socialButton}>
-            <FontAwesome name="google" size={24} color="black" />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.socialButton}>
-            <FontAwesome name="apple" size={24} color="black" />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.socialButton}>
-            <FontAwesome name="facebook" size={24} color="black" />
-          </TouchableOpacity>
-        </View>
-
         <View style={styles.loginSection}>
-          <Text>Already have an account? </Text>
+          <Text style={{ color: 'black' }}>Já tem uma conta? </Text>
           <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-            <Text style={styles.loginText}>Log in</Text>
+            <Text style={styles.loginText}>Entrar</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -80,73 +161,62 @@ export default function SignupScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#6B38FB', // Cor de fundo roxa
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   topSection: {
-    alignItems: 'center',
     marginBottom: 20,
   },
   headerText: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: 'white',
-    marginBottom: 10,
+    color: 'black',
   },
-  image: {
-    width: 150,
-    height: 150,
-    borderRadius: 75,
-  },
-  signupBox: {
+
+  form: {
+    display: 'flex',
     width: '90%',
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 20,
-    alignItems: 'center',
+    height: '60%'
   },
   input: {
     width: '100%',
-    padding: 15,
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 10,
+    marginBottom: 15,
+    color: 'black',
+  },
+  pickerContainer: {
     borderWidth: 1,
     borderColor: '#DDD',
     borderRadius: 10,
     marginBottom: 15,
   },
+  picker: {
+    
+    color: 'black',
+  },
   signupButton: {
-    backgroundColor: '#FFB800', // Cor amarela para o botão
+    backgroundColor: '#7DCD9A',
     paddingVertical: 12,
     paddingHorizontal: 20,
     borderRadius: 10,
-    width: '100%',
     alignItems: 'center',
   },
   signupButtonText: {
     color: 'white',
     fontWeight: 'bold',
   },
-  orText: {
-    marginVertical: 10,
-    color: '#888',
-  },
-  socialButtons: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '60%',
-  },
-  socialButton: {
-    backgroundColor: '#EEE',
-    padding: 10,
-    borderRadius: 10,
-    alignItems: 'center',
-  },
   loginSection: {
     flexDirection: 'row',
     marginTop: 10,
+    justifyContent: 'center',
   },
   loginText: {
-    color: '#6B38FB',
+    color: '#7DCD9A',
     fontWeight: 'bold',
   },
 });
